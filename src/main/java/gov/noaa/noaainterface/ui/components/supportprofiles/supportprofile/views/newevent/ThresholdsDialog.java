cce76484cd8224a322e3654d18748b880e7c63b1
Armando Perea <aleoperea@yahoo.com>
2024-04-30 13:24:16 -0400

    refactor

--- src/main/java/gov/noaa/noaainterface/ui/components/supportprofiles/supportprofile/views/newevent/ThresholdsDialog.java
+++ src/main/java/gov/noaa/noaainterface/ui/components/supportprofiles/supportprofile/views/newevent/ThresholdsDialog.java
@@ -30,23 +30,51 @@ public class ThresholdsDialog extends Dialog {
     private final TextField customLabel = new TextField("Custom Label");
     private final TextArea impactStatement = new TextArea("Impact Statement");
     private final TextArea actions = new TextArea("Actions");
-    private TabManager tabManager = new TabManager();
-
+    private final TabManager tabManager = new TabManager();
     private final Button saveImpactButton = new Button("Save Impact");
     private final Button discardImpactButton = new Button("Discard Impact");
-
     private final VerticalLayout filteredImpactsLayout = new VerticalLayout();
-    private final VerticalLayout recentlyUsedImpacts;
+    private final VerticalLayout recentlyUsedImpacts = new VerticalLayout();
     private final VerticalLayout mainLayout = new VerticalLayout();
-
     private final Binder<Impact> impactBinder = new Binder<>(Impact.class);
+    private final Div pageContainer = new Div();
+    private final Set<Impact> impacts;
     private Consumer<Impact> saveImpactListener;
     private Impact impact;
-    private final Set<Impact> impacts;
-    private final Div pageContainer = new Div();
 
-    public void setSaveImpactListener(Consumer<Impact> saveImpactListener) {
-        this.saveImpactListener = saveImpactListener;
+    public ThresholdsDialog(Set<Impact> impacts) {
+        this.impacts = impacts;
+        initializeComponents();
+        configureBinding();
+        layoutComponents();
+        updateFilteredImpacts(impacts);
+    }
+
+    private void initializeComponents() {
+        title.setText("Impact Details");
+
+        configureComboBox(recentLevel, "Select Level", true, "Level 1", "Level 2", "Level 3", "Level 4", "Level 5");
+        configureComboBox(recentRisk, "Select Risk", true, "Little to none", "Minor", "Moderate", "Significant",
+                "Extreme");
+        configureComboBox(level, "Select Level", true, "Level 1", "Level 2", "Level 3", "Level 4", "Level 5");
+        configureComboBox(risk, "Select Risk", true, "Little to none", "Minor", "Moderate", "Significant", "Extreme",
+                "Custom");
+
+        searchRecentImpacts.setClearButtonVisible(true);
+        customLabel.setVisible(false);
+        customLabel.addValueChangeListener(event -> handleCustomLabelChange(event.getValue()));
+
+        impactStatement.setWidthFull();
+        actions.setWidthFull();
+
+        saveImpactButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveImpactButton.addClickListener(e -> saveImpact());
+        discardImpactButton.addClickListener(e -> resetAndClose());
+
+        searchRecentImpacts.addValueChangeListener(e -> filterImpacts());
+        recentLevel.addValueChangeListener(e -> filterImpacts());
+        recentRisk.addValueChangeListener(e -> filterImpacts());
+        risk.addValueChangeListener(e -> customLabel.setVisible("Custom".equals(risk.getValue())));
     }
 
     public void setImpact(Impact impact) {
@@ -57,31 +85,50 @@ public class ThresholdsDialog extends Dialog {
 
     }
 
-    public ThresholdsDialog(Set<Impact> impacts) {
-        this.impacts = impacts;
-        impactBinder.setBean(impact);
-        setupBinder();
+    private void configureComboBox(ComboBox<String> comboBox, String placeholder, boolean clearButtonVisible,
+            String... items) {
+        comboBox.setItems(items);
+        comboBox.setPlaceholder(placeholder);
+        comboBox.setClearButtonVisible(clearButtonVisible);
+    }
 
-        recentlyUsedImpacts = new VerticalLayout(searchRecentImpacts, new HorizontalLayout(recentLevel, recentRisk),
+    private void configureBinding() {
+        impactBinder.forField(level).asRequired().bind(Impact::getLevel, Impact::setLevel);
+        impactBinder.forField(risk).asRequired().bind(Impact::getRisk, Impact::setRisk);
+        impactBinder.forField(impactStatement).asRequired().bind(Impact::getImpactStatement,
+                Impact::setImpactStatement);
+        impactBinder.forField(actions).asRequired().bind(Impact::getActions, Impact::setActions);
+    }
+
+    private void layoutComponents() {
+        HorizontalLayout levelRiskLayout = new HorizontalLayout(level, risk, customLabel);
+        levelRiskLayout.setWidthFull();
+
+        HorizontalLayout buttonBar = new HorizontalLayout(discardImpactButton, saveImpactButton);
+        buttonBar.setWidthFull();
+        buttonBar.setJustifyContentMode(JustifyContentMode.END);
+
+        recentlyUsedImpacts.add(searchRecentImpacts, new HorizontalLayout(recentLevel, recentRisk),
                 filteredImpactsLayout);
         recentlyUsedImpacts.addClassName(LumoUtility.Background.CONTRAST_5);
         filteredImpactsLayout.setHeight("50vh");
         filteredImpactsLayout.getStyle().set("overflow-y", "auto");
 
-        updateFilteredImpacts(impacts); // Initially display all impacts
+        mainLayout.add(levelRiskLayout, impactStatement, actions, tabManager, pageContainer);
+        mainLayout.setWidth("100%");
+        pageContainer.setWidthFull();
 
-        // Filter logic
-        searchRecentImpacts.addValueChangeListener(e -> filterImpacts());
-        recentLevel.addValueChangeListener(e -> filterImpacts());
-        recentLevel.setPlaceholder("Select");
-        recentLevel.setClearButtonVisible(true);
-        recentRisk.addValueChangeListener(e -> filterImpacts());
-        recentRisk.setPlaceholder("Select");
-        recentRisk.setClearButtonVisible(true);
+        add(title, new HorizontalLayout(recentlyUsedImpacts, mainLayout), buttonBar);
+    }
 
-        configureComponents();
-        layoutComponents();
-
+    private void updateFilteredImpacts(Set<Impact> filteredImpacts) {
+        filteredImpactsLayout.removeAll();
+        filteredImpacts.forEach(impact -> {
+            ImpactSummary impactSummary = new ImpactSummary(impact.getLevel(), impact.getRisk(),
+                    impact.getImpactStatement());
+            impactSummary.getSelectButton().addClickListener(e -> impactBinder.setBean(impact));
+            filteredImpactsLayout.add(impactSummary, new Hr());
+        });
     }
 
     private void filterImpacts() {
@@ -90,94 +137,33 @@ public class ThresholdsDialog extends Dialog {
         String selectedRisk = recentRisk.getValue();
 
         Set<Impact> filtered = impacts.stream()
-                .filter(impact -> (impact.toString().toLowerCase().contains(searchText)) &&
+                .filter(impact -> impact.toString().toLowerCase().contains(searchText) &&
                         (selectedLevel == null || impact.getLevel().equals(selectedLevel)) &&
                         (selectedRisk == null || impact.getRisk().equals(selectedRisk)))
                 .collect(Collectors.toSet());
         updateFilteredImpacts(filtered);
     }
 
-    private void updateFilteredImpacts(Set<Impact> filteredImpacts) {
-        filteredImpactsLayout.removeAll();
-        filteredImpacts.forEach(impact -> {
-            ImpactSummary impactSummary = new ImpactSummary(impact.getLevel(), impact.getRisk(),
-                    impact.getImpactStatement());
-            impactSummary.getSelectButton().addClickListener(e -> {
-                impactBinder.setBean(impact);
-            });
-            filteredImpactsLayout.add(impactSummary);
-            filteredImpactsLayout.add(new Hr());
-        });
+    private void handleCustomLabelChange(String value) {
+        if ("Custom".equals(risk.getValue())) {
+            risk.setItems("Little to none", "Minor", "Moderate", "Significant", "Extreme", value);
+            risk.setValue(value);
+        }
     }
 
-    private void setupBinder() {
-        impactBinder.forField(level).asRequired().bind(Impact::getLevel, Impact::setLevel);
-        impactBinder.forField(risk).asRequired().bind(Impact::getRisk, Impact::setRisk);
-        impactBinder.forField(impactStatement).asRequired().bind(Impact::getImpactStatement,
-                Impact::setImpactStatement);
-        impactBinder.forField(actions).asRequired().bind(Impact::getActions, Impact::setActions);
-
-        saveImpactButton.addClickListener(e -> {
-            if (impactBinder.writeBeanIfValid(impact)) {
-                saveImpactListener.accept(impact);
-                close();
-            }
-        });
-
-        discardImpactButton.addClickListener(e -> {
-            this.impact = new Impact(this.impact.getWeatherHazard());
-            impactBinder.readBean(impact);
+    private void saveImpact() {
+        if (impactBinder.writeBeanIfValid(impact)) {
+            saveImpactListener.accept(impact);
             close();
-        });
+        }
     }
 
-    private void configureComponents() {
-        searchRecentImpacts.setClearButtonVisible(true);
-
-        level.setPlaceholder("Select");
-        level.setClearButtonVisible(true);
-        risk.setPlaceholder("Select");
-        risk.setClearButtonVisible(true);
-
-        impactStatement.setWidthFull();
-        actions.setWidthFull();
-
-        recentLevel.setItems("Level 1", "Level 2", "Level 3", "Level 4", "Level 5");
-        recentRisk.setItems("Little to none", "Minor", "Moderate", "Significant", "Extreme");
-
-        level.setItems("Level 1", "Level 2", "Level 3", "Level 4", "Level 5");
-        risk.setItems("Little to none", "Minor", "Moderate", "Significant", "Extreme", "Custom");
-
-        risk.addValueChangeListener(event -> customLabel.setVisible("Custom".equals(risk.getValue())));
-        customLabel.setVisible(false);
-        customLabel.addValueChangeListener(event -> {
-            String value = event.getValue();
-            if ("Custom".equals(risk.getValue())) {
-                risk.setItems("Little to none", "Minor", "Moderate", "Significant", "Extreme", value);
-                risk.setValue(value);
-            }
-        });
-
-        saveImpactButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
+    private void resetAndClose() {
+        impactBinder.readBean(new Impact(this.impact.getWeatherHazard()));
+        close();
     }
 
-    private void layoutComponents() {
-        HorizontalLayout levelRiskLayout = new HorizontalLayout(level, risk, customLabel);
-        levelRiskLayout.setWidthFull();
-        HorizontalLayout buttonBar = new HorizontalLayout(discardImpactButton, saveImpactButton);
-        buttonBar.setWidthFull();
-        buttonBar.setJustifyContentMode(JustifyContentMode.END); // Flush right
-
-        // Add the pageContainer below the tabs within the main layout
-        mainLayout.add(levelRiskLayout, impactStatement, actions, tabManager, pageContainer);
-        mainLayout.setWidth("100%");
-
-        // Set the width of pageContainer to match the main layout if necessary
-        pageContainer.setWidthFull();
-
-        // Add the main layout to the Dialog
-        add(title, new HorizontalLayout(recentlyUsedImpacts, mainLayout), buttonBar);
+    public void setSaveImpactListener(Consumer<Impact> listener) {
+        this.saveImpactListener = listener;
     }
-
 }
